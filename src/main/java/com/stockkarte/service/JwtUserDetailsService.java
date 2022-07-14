3987ff4140a08d4d439a14d49517bc0fc97dd939
Peter Seubert <peter.seubert@deutschebahn.com>
2022-07-14 16:54:22 +0200

    wip: added basic jwt auth -> not working

---
+++ src/main/java/com/stockkarte/service/JwtUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.stockkarte.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username)throws UsernameNotFoundException {
+        if ("javainuse".equals(username)) {
+            return new User("javainuse", "affe", new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username :: " + username);
+        }
+
+
+    }
+}
